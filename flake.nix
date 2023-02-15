b846d1a3d8a715b293319ee7b57cecfa8d4a59a0
Patrick Himmelmann <himmelmannp@gmail.com>
2023-02-15 23:29:56 +0100

    Nix flake and envrc

---
+++ flake.nix
@@ -0,0 +1,105 @@
+{
+  description = "GUI for tracking your working time";
+
+  # Nixpkgs / NixOS version to use.
+  # inputs.nixpkgs.url = "nixpkgs/nixos-21.05";
+
+  inputs.import-cargo.url = github:edolstra/import-cargo;
+
+  outputs = { self, nixpkgs, import-cargo }:
+    let
+
+      # Generate a user-friendly version number.
+      version = "${builtins.substring 0 8 self.lastModifiedDate}-${self.shortRev or "dirty"}";
+
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
+
+    in {
+
+      # A Nixpkgs overlay.
+      overlay = final: prev: {
+
+        timetrax = with final; final.callPackage ({ inShell ? false }: stdenv.mkDerivation rec {
+          name = "timetrax-${version}";
+
+          # In 'nix develop', we don't need a copy of the source tree
+          # in the Nix store.
+          src = if inShell then null else ./.;
+          
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+          nativeBuildInputs =
+            [ rustc
+              cargo
+              cmake
+              pkg-config
+              fontconfig
+            ] ++ (if inShell then [
+              # In 'nix develop', provide some developer tools.
+              rustfmt
+              rust-analyzer
+              clippy
+            ] else [
+              (import-cargo.builders.importCargo {
+                lockFile = ./Cargo.lock;
+                inherit pkgs;
+              }).cargoHome
+            ]);
+          buildInputs = [
+            sqlite
+            freetype
+            zlib
+            libpng
+            expat
+            bzip2
+            brotli.lib
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXi
+            vulkan-loader
+          ];
+
+          shellHook = ''
+          '';
+
+          target = "--release";
+
+          buildPhase = "cargo build ${target} --frozen --offline";
+
+          doCheck = true;
+
+          checkPhase = "cargo test ${target} --frozen --offline";
+
+          installPhase =
+            ''
+              mkdir -p $out
+              cargo install --frozen --offline --path . --root $out
+              rm $out/.crates.toml
+            '';
+        }) {};
+
+      };
+
+      # Provide some binary packages for selected system types.
+      packages = forAllSystems (system:
+        {
+          inherit (nixpkgsFor.${system}) timetrax;
+        });
+
+      # The default package for 'nix build'. This makes sense if the
+      # flake provides only one package or there is a clear "main"
+      # package.
+      defaultPackage = forAllSystems (system: self.packages.${system}.timetrax);
+
+      # Provide a 'nix develop' environment for interactive hacking.
+      devShell = forAllSystems (system: self.packages.${system}.timetrax.override { inShell = true; });
+
+    };
+}
